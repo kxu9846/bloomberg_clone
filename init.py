9b571c1758c95f64044c265ff86244aae40946f0
Kitty Xu <kittyxu@Kittys-MBP.home>
2021-05-24 15:02:38 -0400

    env setup

--- init.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='bloomberg_clone',
-    version='0.1.0',
-    packages=find_packages(include=['bloomberg_clone', 'bloomberg_clone.*'])
-)
